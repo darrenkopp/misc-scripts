410b502c878c861d3308a154e96675d3e9c17bc3
Darren Kopp <darrenkopp@gmail.com>
2012-09-11 10:18:41 -0600

    Friendlier Output
    Not necessarily great output, but a bit friendlier.

--- asp.net/maintenance/cleanup-aspnet-temporary-files.ps1
+++ asp.net/maintenance/cleanup-aspnet-temporary-files.ps1
@@ -1,8 +1,19 @@
 ﻿# array of servers we want to run this command against. You can add the servers to the list here, or read them from a file, etc
 $servers = @("server-01","server-02");
 
+$machineStatusLookup = New-Object 'System.Collections.Generic.SortedDictionary[string,object]';
+
 # run the command against all configured servers
 invoke-command -cn $servers {
+
+    function Write-Output-Message($message) {
+        $output = New-Object PSObject -Property @{
+            Message = $message
+        };
+
+        Write-Output $output;
+    }
+    
     # List of all versions of .net that have Temporary ASP.NET Files folders
     $versions = @("v2.0.50727","v4.0.30319");
     $frameworks = @("Framework","Framework64");
@@ -17,7 +28,9 @@ invoke-command -cn $servers {
 
     #regex for matching file in the __AssemblyInfo.ini__ file
     $rgx = [regex]'(?i)file:///[\w\d\._\-/\:]+';
-    
+
+    # counter variable to track how many deletions we have done.
+    $folder_deletions = 0;
 
     # iterate every version specified
     $versions | %{ 
@@ -30,7 +43,6 @@ invoke-command -cn $servers {
 
             # progress variables
             $scan_position = 0;
-            $folder_deletions = 0;
 
             # recursively search for __AssemblyInfo__.ini files which contain information about cached dll
             ls $aspnet_temp -r -filter __AssemblyInfo__.ini | %{
@@ -71,11 +83,31 @@ invoke-command -cn $servers {
 
                 # write some info messages every 1000 files or so
                 if (($scan_position % 1000) -eq 0) {
-                    Write-Output "[$machine] Scanned: $scan_position. Deleted: $folder_deletions.";
+                    $msg = "[$framework\$version] Scanned: $scan_position. Deleted: $folder_deletions.";
+                    Write-Output-Message $msg;
                 }
             }
         }
     }
-}
+
+    Write-Output-Message "Done.";
+} | %{
+    $source = $_.PSComputerName;
+    $message = $_.Message;
+
+    $status = $null;
+    if ($machineStatusLookup.TryGetValue($source, [ref]$status) -ne $true) {
+        $status = New-Object PSObject -Property @{ Server=$source; Status=$message; };
+
+        $machineStatusLookup.Add($source, $status);
+    }
+
+    $status.Server = $source;
+    $status.Status = $message;
+
+    # clear output and re-draw
+    Clear-Host;
+    $machineStatusLookup | select -ExpandProperty Values | Format-Table -Property Status, Server;
+};
 
 write-output "Done";
